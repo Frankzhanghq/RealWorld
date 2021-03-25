c3b68a00f0f1ee6ddc0cfc618b9941d031ca5f3a
zhanghuaqiang <zhanghuaqiang@swifthealth.cn>
2021-03-25 16:19:10 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>hello Nuxt</h1>
+        <h2>{{ name }}</h2>
+        <h2>{{ age }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "index",
+    asyncData(context) {
+        console.log(context);
+        console.log(process.server);
+        return { name: "zhangsan" };
+    },
+    data() {
+        return {
+            age: 18,
+        };
+    },
+};
+</script>
+
+<style>
+</style>
